d3598bdd9d13ffdb0c65937fbb861116b435c915
GaoXiangYa <gx19991105@outlook.com>
2025-09-14 16:28:14 +0800

    add feml_log

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,30 @@
-mod utils;
+pub mod log;
+pub mod pad;
 
-pub use utils::FEML_MEM_ALIGN;
+pub use pad::FEML_MEM_ALIGN;
+
+#[derive(Debug, Clone, Copy)]
+pub enum FemlLogLevel {
+    None,
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Cont, // continue previous log
+}
+
+// fn fn_name_of<T>(_: T) -> &'static str {
+//     std::any::type_name::<T>()
+// }
+
+// macro_rules! fn_name {
+//     () => {{
+//         fn f() {}
+//         let full = fn_name_of(f);
+//         &full[..full.len() - 3]
+//     }};
+// }
+
+// fn feml_log_internal(log_level: FemlLogLevel, file: &str, line: u32, args: fmt::Arguments) {
+//     eprintln!("{} : {} : {} : {}", file, line, fn_name!(), args);
+// }
