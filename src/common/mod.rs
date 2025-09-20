33d61ed79b1db2d8a25618f884620dc935549913
GaoXiangYa <gx19991105@outlook.com>
2025-09-20 20:00:07 +0800

    add type_traits

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,6 +1,4 @@
 pub mod context;
 pub mod def;
 pub mod tensor;
-pub mod type_traits;
-
 mod buf;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -3,3 +3,6 @@ pub mod types;
 pub use types::FemlObjectType;
 pub use types::TensorType;
 pub use types::FemlOpType;
+pub mod type_traits;
+
+pub use types::FEML_TYPE_COUNT;
